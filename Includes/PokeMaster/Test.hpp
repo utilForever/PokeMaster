f42d490fd1410c7851b2dd9241b46194ee68e176
Chris Ohk <utilForever@gmail.com>
2020-05-18 14:03:13 +0900

    feat(project-setup): Create CMake based project

---
+++ Includes/PokeMaster/Test.hpp
@@ -0,0 +1,6 @@
+#ifndef POKE_MASTER_TEST_HPP
+#define POKE_MASTER_TEST_HPP
+
+int Add(int a, int b);
+
+#endif
---
+++ Sources/PokeMaster/Test.cpp
@@ -0,0 +1,6 @@
+#include <PokeMaster/Test.hpp>
+
+int Add(int a, int b)
+{
+    return a + b;
+}
