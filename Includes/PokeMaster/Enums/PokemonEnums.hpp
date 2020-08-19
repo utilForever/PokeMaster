c984c7673710fe001fe9808e7d9844ba35738746
Chris Ohk <utilforever@gmail.com>
2020-08-19 21:54:29 +0900

    refactor: Correct the order of enum class 'Nature'

--- Includes/PokeMaster/Enums/PokemonEnums.hpp
+++ Includes/PokeMaster/Enums/PokemonEnums.hpp
@@ -47,37 +47,37 @@ enum class Stat
 
 //! Pokemon natures.
 //! First digit of nature code indicates a stat which will be increased by 10%.
-//! Second digit of nature Code indicates a stat which will be decreased by 10%.
+//! Second digit of nature code indicates a stat which will be decreased by 10%.
 //! Numbers 1~5 correspond to ATTACK, DEFENSE, SPECIAL_ATTACK, SPECIAL_DEFENSE
 //! and SPEED.
 enum class Nature
 {
     INVALID,
-    ADAMANT = 13,  //!      +ATTACK            -SPECIAL_ATTACK
-    BASHFUL = 0,
-    BOLD = 21,     //!      +DEFENSE           -ATTACK
-    BRAVE = 15,    //!      +ATTACK            -SPEED
-    CALM = 41,     //!      +SPECIAL_DEFENSE   -ATTACK
-    CAREFUL = 43,  //!      +SPECIAL_DEFENSE   -SPECIAL_ATTACK
-    DOCILE = 0,
-    GENTLE = 42,  //!      +SPECIAL_DEFENSE   -DEFENSE
-    HARDY = 0,
-    HASTY = 52,    //!      +SPEED             -DEFENSE
-    IMPISH = 23,   //!      +DEFENSE           -SPECIAL_ATTACK
-    JOLLY = 53,    //!      +SPEED             -SPECIAL_ATTACK
-    LAX = 24,      //!      +DEFENSE           -SPECIAL_DEFENSE
-    LONELY = 12,   //!      +ATTACK            -DEFENSE
-    MILD = 32,     //!      +SPECIAL_ATTACK    -DEFENSE
-    MODEST = 31,   //!      +SPECIAL_ATTACK    -ATTACK
-    NAIVE = 54,    //!      +SPEED             -SPECIAL_DEFENSE
-    NAUGHTY = 14,  //!      +ATTACK            -SPECIAL_DEFENSE
-    QUIET = 35,    //!      +SPECIAL_ATTACK    -SPEED
-    QUIRKY = 0,
-    RASH = 34,     //!      +SPECIAL_ATTACK    -SPECIAL_DEFENSE
-    RELAXED = 25,  //!      +DEFENSE           -SPEED
-    SASSY = 45,    //!      +SPECIAL_DEFENSE   -SPEED
-    SERIOUS = 0,
-    TIMID = 52  //!      +SPEED             -ATTACK
+    HARDY,
+    BOLD,
+    MODEST,
+    CALM,
+    TIMID,
+    LONELY,
+    DOCILE,
+    MILD,
+    GENTLE,
+    HASTY,
+    ADAMANT,
+    IMPISH,
+    BASHFUL,
+    CAREFUL,
+    RASH,
+    JOLLY,
+    NAUGHTY,
+    LAX,
+    QUIRKY,
+    NAIVE,
+    BRAVE,
+    RELAXED,
+    QUIET,
+    SASSY,
+    SERIOUS
 };
 
 //! Pokemon status.
